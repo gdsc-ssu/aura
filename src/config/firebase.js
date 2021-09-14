6f7e5e1a71959e34e10b50f6d503b0c4e954353c
gomjellie <gomjellie@gmail.com>
2021-09-14 17:01:41 +0900

    update firebaseConfig, vue.config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,25 +1,30 @@
 import firebase from 'firebase/app';
-import 'firebase/auth';   
+import 'firebase/auth';
 import 'firebase/storage';
 import 'firebase/firestore';
 import 'firebase/messaging';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOl0qzkhCDmyWeeIT8AZe4xyJJlIfU5gE",
-  authDomain: "myaurapp.firebaseapp.com",
-  databaseURL: "https://myaurapp.firebaseio.com",
-  projectId: "myaurapp",
-  storageBucket: "myaurapp.appspot.com",
-  messagingSenderId: "419511997060",
-  appId: "1:419511997060:web:ce7aa0d308f6b484f868d1"
+  apiKey: 'AIzaSyBMwzqRRI8JV40M5U0XVj9WL3B2f47Zz0g',
+  authDomain: 'gdsc-ssu.firebaseapp.com',
+  projectId: 'gdsc-ssu',
+  storageBucket: 'gdsc-ssu.appspot.com',
+  messagingSenderId: '1025647897879',
+  appId: '1:1025647897879:web:1480039c4eb7d9aeca5b52',
+  measurementId: 'G-WB9SKLWV4G',
 };
 
 firebase.initializeApp(firebaseConfig);
-firebase.firestore().enablePersistence({synchronizeTabs:!0}).catch(()=>{console.warn("DB offline support not available")})
-export default{
-  notificationSupported:firebase.messaging.isSupported(),
-  messaging: (firebase.messaging.isSupported())?firebase.messaging():null,
+firebase
+  .firestore()
+  .enablePersistence({ synchronizeTabs: !0 })
+  .catch(() => {
+    console.warn('DB offline support not available');
+  });
+export default {
+  notificationSupported: firebase.messaging.isSupported(),
+  messaging: firebase.messaging.isSupported() ? firebase.messaging() : null,
   firestore: firebase.firestore(),
-  auth:firebase.auth(),
-  storage:firebase.storage(),
+  auth: firebase.auth(),
+  storage: firebase.storage(),
 };
