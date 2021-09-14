1f52f4803f222193737d44a2d4000040230c1141
gomjellie <gomjellie@gmail.com>
2021-09-14 20:51:45 +0900

    Team 한글화

--- src/components/team/TeamHeader.vue
+++ src/components/team/TeamHeader.vue
@@ -1,24 +1,45 @@
 <template>
-    <v-container fluid class="py-0 my-0 mt-2">
-        <v-row align="center" class="py-0 my-0">
-            <v-col md="12" sm="12" cols="12" class="py-0 my-0">
-                <p class="google-font mb-0" style="font-weight: 350;font-size:200%"><b>
-                    <span style="color: #1a73e8;">Our</span> Team </b> 
-                </p>
-                <p class="google-font mt-0 hidden-sm-and-down">Google is known all around the world. Everyone is 'googling', checking on 'maps' and communicating in 'gmail'. For simple users, they are services that just works, but not for us. Developers see much more: APIs, scalability issues, complex technology stacks. And that is what GDG is about.</p>
-                <p class="google-font mt-0 ">Our goal is to organize space to connect the best industry experts with Indian audience to boost development of IT. And Our Core Team is:</p>
-                
-            </v-col>
-        </v-row>
-    </v-container>
+  <v-container fluid class="py-0 my-0 mt-2">
+    <v-row align="center" class="py-0 my-0">
+      <v-col md="12" sm="12" cols="12" class="py-0 my-0">
+        <p class="google-font mb-0" style="font-weight: 350;font-size:200%">
+          <b> <span style="color: #1a73e8;">Our</span> Team </b>
+        </p>
+        <p class="google-font mt-0 hidden-sm-and-down">
+          그냥 학생이라고 하기엔 뭔가 독특합니다.
+        </p>
+        <p class="google-font mt-0 ">
+          SW 분야를 전공하는 학생으로 자신만의 꿈과 목표가 명확하며 그저 배우는
+          것 뿐만이 아닌, 자신만의 주력분야를 가지고 실질적인 SW 개발/디자인을
+          진행합니다.
+        </p>
+        <p class="google-font mt-0 ">
+          우리는 우리 스스로를 평범한 학생이 아닌 학생 개발자/디자이너라고
+          칭합니다.
+        </p>
+        <p>
+          CS 전공자들에게 널리 알려진 SW Maestro 과정이나 삼성 소프트웨어
+          멤버십, Best of Best와 같은 연수과정을 거친 학생들이 이곳에
+          모여있습니다.
+        </p>
+        <p>
+          그리고 대학을 졸업하지 않았음에도 자신만의 아이템을 가지고 SW 창업을
+          진행하거나,
+        </p>
+        <p>
+          우리가 익히들은 NAVER / 카카오 / 라인 / 쿠팡 / 배달의민족 등의
+          기업체에서 활약을 하는 능력자가 있습니다.
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-    name:'TeamHeader',
-    data() {
-        return {
-        }
-    }
-}
+  name: 'TeamHeader',
+  data() {
+    return {};
+  },
+};
 </script>
