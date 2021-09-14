c1ee725857e958aa5d7611721a9437d4327efea4
gomjellie <gomjellie@gmail.com>
2021-09-14 20:42:29 +0900

    Home 한글화

--- src/components/home/WhatWeDo.vue
+++ src/components/home/WhatWeDo.vue
@@ -2,23 +2,15 @@
   <v-container fluid>
     <v-row align="center" justify="center">
       <v-col cols="12" md="3" lg="3" sm="4" class>
-        <p class="google-font mb-0" style="font-size:150%">What we do?</p>
-        <p
-          class="google-font mt-0"
-          style="font-size:95%"
-        >Google Developer Groups (GDGs) are for developers who are interested in Google's developer technology.</p>
-        <p class="google-font" style="font-size:95%">About different Google technologies</p>
-        <span v-for="(item,i) in whatwedodata" :key="i">
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on }">
-              <a :href="item.link" target="_blank" class="ma-0 mx-3" rel="noreferrer">
-                <img :src="getImgUrl(item.image)" width="25vh" alt srcset dark v-on="on" />
-              </a>
-              &nbsp;&nbsp;&nbsp;
-            </template>
-            <span>{{item.TechName}}</span>
-          </v-tooltip>
-        </span>
+        <p class="google-font mb-0" style="font-size:150%">
+          Mission of GDSC SSU
+        </p>
+        <p class="google-font mt-0" style="font-size:95%">
+          GDSC SSU는 최고의 개발자를 모으고, 기술을 전파하고, 함께 배우고
+          성장하며 학창 시절의 추억을 만드는것을 미션으로 여깁니다.
+        </p>
+        <p class="google-font" style="font-size:95%"></p>
+        <span v-for="(item, i) in whatwedodata" :key="i"> </span>
       </v-col>
       <v-col cols="12" md="9" lg="9" sm="8">
         <v-container fluid>
@@ -26,34 +18,37 @@
             <v-col md="3" lg="3" sm="6" cols="6" class="text-center pa-2">
               <v-icon large style="font-size:300%">mdi-comment</v-icon>
               <p class="google-font mt-2" style="font-size:130%">Talks</p>
-              <p
-                class="google-font"
-                style="font-size:95%"
-              >Get updated with the latest news and announcements</p>
+              <p class="google-font" style="font-size:95%">
+                커뮤니티 멤버들과 함께 기술적인 교류와 소통, 네트워킹 기회를
+                가져봐요!
+              </p>
             </v-col>
             <v-col md="3" lg="3" sm="6" cols="6" class="text-center pa-2">
               <v-icon large style="font-size:300%">mdi-code-tags</v-icon>
               <p class="google-font mt-2" style="font-size:130%">Codelabs</p>
-              <p
-                class="google-font"
-                style="font-size:95%"
-              >Get hands-on experience and guidance from the community members</p>
+              <p class="google-font" style="font-size:95%">
+                Get hands-on experience and guidance from the community members
+              </p>
             </v-col>
             <v-col md="3" lg="3" sm="6" cols="6" class="text-center pa-2">
               <v-icon large style="font-size:300%">mdi-school</v-icon>
-              <p class="google-font mt-2" style="font-size:130%">Campus Roadshows</p>
-              <p
-                class="google-font"
-                style="font-size:95%"
-              >Share knowledge in different Companies, colleges and universities</p>
+              <p class="google-font mt-2" style="font-size:130%">
+                Campus Roadshows
+              </p>
+              <p class="google-font" style="font-size:95%">
+                Share knowledge in different Companies, colleges and
+                universities
+              </p>
             </v-col>
             <v-col md="3" lg="3" sm="6" cols="6" class="text-center pa-2">
               <v-icon large style="font-size:300%">mdi-chart-bubble</v-icon>
-              <p class="google-font mt-2" style="font-size:130%">Live Viewing Parties</p>
-              <p
-                class="google-font"
-                style="font-size:95%"
-              >Share knowledge in different Companies, colleges and universities</p>
+              <p class="google-font mt-2" style="font-size:130%">
+                Live Viewing Parties
+              </p>
+              <p class="google-font" style="font-size:95%">
+                Share knowledge in different Companies, colleges and
+                universities
+              </p>
             </v-col>
           </v-row>
         </v-container>
@@ -63,20 +58,20 @@
 </template>
 
 <script>
-import whatwedodata from "@/assets/data/whatwedo.json";
+import whatwedodata from '@/assets/data/whatwedo.json';
 export default {
-  name: "App",
+  name: 'App',
   data: () => ({
-    whatwedodata: whatwedodata
+    whatwedodata: whatwedodata,
   }),
   methods: {
     getImgUrl(url) {
       if (url.length > 0) {
-        return require("@/assets/img/what-we-do/" + url);
+        return require('@/assets/img/what-we-do/' + url);
       } else {
-        return require("@/assets/img/what-we-do/notFound.png");
+        return require('@/assets/img/what-we-do/notFound.png');
       }
-    }
-  }
+    },
+  },
 };
 </script>
